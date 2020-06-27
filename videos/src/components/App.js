ace0d92190b9de45c0ef78f1dd0ed1d6b6ad787b
Muhammad Majid <mmajidkamel@gmail.com>
2020-06-27 22:58:24 +0200

    search bar component

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  
   render () {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar />
       </div>
     );
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: ' '};
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: call callback from parent component
+  }
+
   render () {
     return (
-      <div>Sreach bar</div>
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Videos Search: </label>
+            <input type="text" value={this.state.term} onChange={this.onInputChange} />
+          </div>
+        </form>
+      </div>
     )
   }
 }
