ea4393623cdfd1c817891e2d824102b1a1a35553
Muhammad Majid <mmajidkamel@gmail.com>
2020-07-04 20:15:17 +0200

    throtling and delayed requests when user type search query

--- widgets-hooks/src/components/Search.js
+++ widgets-hooks/src/components/Search.js
@@ -1,8 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
+const WIKIPEDIA_BASE_URL = 'https://en.wikipedia.org/w/api.php';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+
+    const search = async () => {
+      const { data } = await Axios.get(WIKIPEDIA_BASE_URL, {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
+
+      setResults(data.query.search);
+    }
+    
+    // throttle, when we do search
+    const timeOutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 750);
+
+    // cleanup function
+    return () => {
+      clearTimeout(timeOutId);
+    }
+
+  }, [term]);
+
+  const redneredResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a 
+            className="ui button"
+            href={`https://en.wikipedia.org/?curid=${result.pageid}`}
+          >
+          Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div>
@@ -16,6 +70,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{redneredResults}</div>
     </div>
   )
 }
