be218a5a6646fdd7265719deea54096398dbb7b7
Muhammad Majid <mmajidkamel@gmail.com>
2020-07-24 18:42:32 +0200

    wrapping app with Provider component holding ref to redux store

--- songs-redux/src/index.js
+++ songs-redux/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={ createStore(reducers) }>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
