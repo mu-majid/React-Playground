50ff94d1e7d305933b5ca5430a81578fe45fcdb7
Muhammad Majid <mmajidkamel@gmail.com>
2020-07-24 17:16:41 +0200

    using redux to alter and fetch app state

--- Redux-Playground/index.js
+++ Redux-Playground/index.js
@@ -2,3 +2,19 @@ import { createStore, combineReducers } from '@reduxjs/toolkit';
 import { createClaim, createPolicy, deletePolicy } from './ActionCreators';
 import { accountingReducer, claimsHistoryReducer, policyReducer } from './Reducers';
 
+const ourDepts = combineReducers(
+  {
+    accountingReducer, claimsHistoryReducer, policyReducer
+  }
+);
+
+const store = createStore(ourDepts);
+
+console.log(store.dispatch(createPolicy('Julia', 150)));
+console.log(store.dispatch(createPolicy('Lea', 50)));
+
+console.log(store.getState());
+
+console.log(store.dispatch(createClaim('Lea', 125)));
+
+console.log(store.getState());
