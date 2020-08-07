acda0dc8c7b38bc3f1c0c83734e882d0d0ab2014
Muhammad Majid <mmajidkamel@gmail.com>
2020-08-07 14:36:15 +0200

    solving over-fetching with memoization

--- basic-blog-with-redux/src/actions/index.js
+++ basic-blog-with-redux/src/actions/index.js
@@ -1,7 +1,20 @@
-import jsonPlaceholder  from '../apis/jsonPlaceholder';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
+
 
 export const fetchPosts = () => async dispatch => {
   const { data } = await jsonPlaceholder.get('/posts');
 
   dispatch({ type: 'FETCH_POSTS', payload: data });
 };
+
+export const fetchUser = (id) => async (dispatch, ) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+
+  const { data } = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: data });
+})
