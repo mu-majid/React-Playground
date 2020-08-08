578130a81f999cf497aeb3478de2b504dae28ca5
Muhammad Majid <mmajidkamel@gmail.com>
2020-08-08 15:37:13 +0200

    react-router-dom using <a> as navigation tool which is bad

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+// <a> causes full page(App) reload and all redux data are wiped out.
+const pageOne = () => {
+  return  (
+    <div>
+      Page pageOne
+      BAD NAVIGATION
+      <a href="/pagetwo" >Navigate to page two</a>
+    </div>
+  );
+}
+
+const pageTwo = () => {
+  return  (
+    <div>
+      Page pageTwo
+      BAD NAVIGATION
+
+      <a href="/" >Navigate to page one</a>
+    </div>
+  );
+}
 
 const App = (props) => {
   return (
-    <div>App.</div>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={pageOne}></Route>
+          <Route path="/pageTwo" exact component={pageTwo}></Route>
+          
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
