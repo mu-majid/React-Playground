757fd6a0ace09962f5a190f42818aa298861422f
Muhammad Majid <mmajidkamel@gmail.com>
2020-08-11 22:13:37 +0200

    update auth state by listening to auth status on gapi

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -12,8 +12,13 @@ class GoogleAuth extends React.Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         })
     });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   }
 
   renderAuthButton () {
