8c6a5703c7a347042cfb662a1454a4b2385fa2f2
Muhammad Majid <mmajidkamel@gmail.com>
2020-08-20 23:50:04 +0200

    working signIn and signOut

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -20,6 +20,14 @@ class GoogleAuth extends React.Component {
   onAuthChange = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   }
+  
+  onSignInClick = () => {
+    this.auth.signIn();
+  }
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  }
 
   renderAuthButton () {
     if (this.state.isSignedIn === null) {
@@ -28,7 +36,7 @@ class GoogleAuth extends React.Component {
     // TODO: Add Buttons functionality
     else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button" >
+        <button onClick={this.onSignOutClick} className="ui red google button" >
           <i className="google icon">
             Sign Out
           </i>
@@ -37,7 +45,7 @@ class GoogleAuth extends React.Component {
     }
     else {
       return (
-        <button className="ui red google button" >
+        <button onClick={this.onSignInClick} className="ui red google button" >
           <i className="google icon">
             Sign In
           </i>
