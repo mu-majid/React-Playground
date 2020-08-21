ed9da09d226d0ddeb8efbe9d19bf5d80f093f415
Muhammad Majid <mmajidkamel@gmail.com>
2020-08-21 02:51:32 +0200

    adding redux dev tools middleware

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducers from './Reducers';
 import App from './components/App';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
