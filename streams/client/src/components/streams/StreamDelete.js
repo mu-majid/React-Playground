885daafb65e55ef35c2c45f300a828cfcb2df325
Muhammad Majid <mmajidkamel@gmail.com>
2020-08-26 22:18:09 +0200

    wip delete button functionality

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import Modal from '../Modal';
+import { connect } from 'react-redux';
+import { fetchStream, deleteStream } from '../../Actions';
 
-const StreamDelete = () => {
-  const actions = (
-    // because if div is used the styling will be messed up, also fragment is not rendered on html
-    <React.Fragment>
-      <button className="ui button negative" >Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  )
-  return (
-    <div>
-      StreamDelete
-      <Modal
-        title="Delete Stream"
-        content="Are you Sure You want delete this stream?"
-        actions={actions}
-      />
-    </div>
-  )
+class StreamDelete extends React.Component {
+
+  componentDidMount () {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render () {
+    const actions = (
+      // because if div is used the styling will be messed up, also fragment is not rendered on html
+      <React.Fragment>
+        <button className="ui button negative" >Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    )
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title="Delete Stream"
+          content={`Are you Sure You want delete \`${this.props.stream.title}\` stream?`}
+          actions={actions}
+        />
+      </div>
+    );
+  }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -16,9 +16,9 @@ class StreamList extends React.Component {
           <Link to={`streams/edit/${stream.id}`} className="ui button primary">
             Edit
           </Link>
-          <button className="ui button negative">
+          <Link to={`streams/delete/${stream.id}`} className="ui button negative">
             Delete
-          </button>
+          </Link>
         </div>
       )
     }
