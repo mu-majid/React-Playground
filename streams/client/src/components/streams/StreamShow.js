c39693bcebf770cd2643735b253c79137da21351
Muhammad Majid <mmajidkamel@gmail.com>
2020-08-28 22:02:29 +0200

    showing video player and streaming from OBS

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../Actions';
+import flv from 'flv.js';
 
 class StreamShow extends React.Component {
 
+  constructor (props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
+  buildPlayer () {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.flvPlayer = flv.createPlayer(
+      {
+        type: 'flv',
+        url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+      }
+    );
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
+  }
+
   componentDidMount () {
     this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate () {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount () {
+    this.flvPlayer.destroy();
   }
 
   render () {
@@ -19,6 +49,7 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%"}} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
